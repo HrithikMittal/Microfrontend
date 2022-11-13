3b41ead024d095690c060310e640867895f948ba
Adhikansh Mittal <adhikansh.mittal@bajajfinserv.in>
2022-11-13 08:37:36 +0530

    add history

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Route, Switch, Router } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
@@ -12,16 +12,16 @@ const generateClassName = createGenerateClassName({
   productionPrefix: "ma",
 });
 
-const App = () => {
+const App = ({ history }) => {
   return (
     <div>
       <StylesProvider generateClassName={generateClassName}>
-        <BrowserRouter>
+        <Router history={history}>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
             <Route path="/" component={Landing} />
           </Switch>
-        </BrowserRouter>
+        </Router>
       </StylesProvider>
     </div>
   );
